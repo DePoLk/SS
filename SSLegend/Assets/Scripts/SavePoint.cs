bf99d13d53ee60061b6fd764f3c80c95f36c0d50
DePoLK <ms0507030@gmail.com>
2019-04-22 19:17:10 +0800

    打架

--- SSLegend/Assets/Scripts/SavePoint.cs
+++ SSLegend/Assets/Scripts/SavePoint.cs
@@ -39,7 +39,8 @@ public class SavePoint : MonoBehaviour {
         ExpS = FindObjectOfType<Exp_System>();
         MenuTweener.SetAutoKill(true);
         SaveMenu = GameObject.Find("MenuBG").GetComponent<Image>();
-	}
+        SaveGameBtn = GameObject.Find("SaveGame").GetComponent<Button>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
